6088b05528239e9ca459e99b2004fb69e792a3ec
animesh <animesh317kumar@gmail.com>
2023-04-04 12:30:17 +0530

    Added date picker dialog for exp date

--- app/src/main/java/com/example/inventorymanagement_101/Dashborad2Activity.java
+++ app/src/main/java/com/example/inventorymanagement_101/Dashborad2Activity.java
@@ -2,16 +2,20 @@ package com.example.inventorymanagement_101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class Dashborad2Activity extends AppCompatActivity {
@@ -21,6 +25,8 @@ public class Dashborad2Activity extends AppCompatActivity {
     ListView lv_productList;
     ArrayAdapter productArrayAdapter;
     DatabaseHelper databaseHelper;
+    DatePickerDialog datePickerDialog;
+    Button btn_expiryDatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +39,15 @@ public class Dashborad2Activity extends AppCompatActivity {
         et_threshold = findViewById(R.id.et_threshold);
         btn_viewAll = findViewById(R.id.btn_viewAll);
         btn_Add = findViewById(R.id.btn_add);
+        btn_expiryDatePicker = findViewById(R.id.btn_expiryDatePicker);
         lv_productList = findViewById(R.id.lv_productList);
 
         databaseHelper = new DatabaseHelper(Dashborad2Activity.this);
 
         ShowProductOnListView(databaseHelper);
+
+        initDatePicker();
+        btn_expiryDatePicker.setText(getTodaysDate());
 
         //listners
         btn_Add.setOnClickListener(new View.OnClickListener() {
@@ -119,5 +129,81 @@ public class Dashborad2Activity extends AppCompatActivity {
     private void ShowProductOnListView(DatabaseHelper databaseHelper) {
         productArrayAdapter = new ArrayAdapter<productModel> (Dashborad2Activity.this, android.R.layout.simple_list_item_1, databaseHelper.getAll());
         lv_productList.setAdapter(productArrayAdapter);
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                btn_expiryDatePicker.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = android.R.style.Theme_Holo_Light_Dialog_MinWidth;
+
+        datePickerDialog = new DatePickerDialog(Dashborad2Activity.this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()-1000);   //subtracted 1 sec from current time
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
     }
 }
